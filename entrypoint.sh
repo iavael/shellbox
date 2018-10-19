3f75c8496ad811f1a5a63cf7c977b8463c858642
Iavael <iavael@users.noreply.github.com>
2018-10-19 22:27:43 +0300

    Remove unneeded code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#if [ ! -e /etc/dropbear/dropbear_ecdsa_host_key ]; then
-#  /usr/bin/dropbearkey -t ecdsa -f /etc/dropbear/dropbear_ecdsa_host_key
-#fi
-
 echo "shell:${PASSWD:-shell}" |chpasswd
 
 /usr/sbin/dropbear -w -R -F -E $@
